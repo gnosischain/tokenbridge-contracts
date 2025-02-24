f40aa1ed398f408f3cee2d6bbba9a0d72c1bc99e
zengzengzenghuy <tayzh2799@gmail.com>
2025-02-24 12:10:08 +0100

    chore(script): update Deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,13 +12,14 @@ contract DeployScript is Script{
        function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address bridgeOwner = vm.envAddress("BRIDGE_OWNER");
+        address proxyAdminOwner = vm.envAddress("PROXY_ADMIN_OWNER");
         vm.startBroadcast(deployerPrivateKey);
 
         // Deployment for xDAIForeignBridge can only use forge create because of the incompatible Solidity version (0.4.24 for xDAIForeignBridge)
         // refer to deploy.sh
 
         BridgeRouter router = new BridgeRouter();
-        TransparentUpgradeableProxy bridgeRouterProxy = new TransparentUpgradeableProxy(address(router),bridgeOwner,  abi.encodeWithSignature("initialize()"));
+        TransparentUpgradeableProxy bridgeRouterProxy = new TransparentUpgradeableProxy(address(router), proxyAdminOwner,  abi.encodeWithSignature("initialize(address)",bridgeOwner));
         XDaiBridgePeripheral peripheral = new XDaiBridgePeripheral(address(bridgeRouterProxy));
 
         
